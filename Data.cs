e81703cea94bc9470a70d390346f1994a1e1547f
Tezikov Roman <tez.romach@gmail.com>
2017-03-20 16:27:06 +0300

    add triggers which is called before a field is changed

--- Data.cs
+++ Data.cs
@@ -1,259 +1,334 @@
-﻿// DataKeeper v1.3
+﻿// DataKeeper v1.4
 // created by TezRomacH
 
 using System;
 using System.Collections.Generic;
 
-public sealed class Data
+namespace DataKeeper
 {
-    private Dictionary<string, object> data;
-    private Dictionary<string, List<Action>> bindedActions;
-
-    private static volatile Data instance;
-    private Data()
+    [Flags]
+    public enum TriggerKind
     {
-        data = new Dictionary<string, object>();
-        bindedActions = new Dictionary<string, List<Action>>();
+        Before = 1,
+        After = 2
     }
 
-    private static object syncRoot = new object();
-
-    public static Data Instance
+    public sealed class Data
     {
-        get
+        private class DataInfo
         {
-            if (instance == null)
+            public object Value { get; set; }
+            public List<Action> TriggersBefore { get; set; }
+            public List<Action> TriggersAfter { get; set; }
+
+            public void AddTriggers(Action action, TriggerKind kind)
             {
-                lock (syncRoot)
+                if (kind.TriggerHasFlag(TriggerKind.Before))
                 {
-                    if (instance == null)
-                        instance = new Data();
+                    if (TriggersBefore == null)
+                        TriggersBefore = new List<Action>();
+
+                    TriggersBefore.Add(action);
+                }
+
+                if (kind.TriggerHasFlag(TriggerKind.After))
+                {
+                    if (TriggersAfter == null)
+                        TriggersAfter = new List<Action>();
+
+                    TriggersAfter.Add(action);
                 }
             }
-            return instance;
-        }
-    }
 
-    #region binding methods 
-    /// <summary>
-    /// Связывает выполнение действия action при изменении значения данных ко ключу key
-    /// </summary>
-    public void BindChangeField(string key, Action action)
-    {
-        List<Action> actions = null;
-        if (bindedActions.TryGetValue(key, out actions))
+            public void RemoveTriggers(TriggerKind kind)
+            {
+                if (kind.TriggerHasFlag(TriggerKind.Before))
+                    TriggersBefore = null;
+
+                if (kind.TriggerHasFlag(TriggerKind.After))
+                    TriggersAfter = null;
+            }
+        }
+
+        private Dictionary<string, DataInfo> data;
+
+        private static volatile Data instance;
+
+        private Data()
         {
-            actions?.Add(action);
-            return;
+            data = new Dictionary<string, DataInfo>();
         }
 
-        bindedActions[key] = new List<Action> { action };
-    }
+        private static object syncRoot = new object();
 
-    /// <summary>
-    /// Связывает выполнение всех действий actions при изменении значения данных ко ключу key
-    /// </summary>
-    /// <param name="key"></param>
-    /// <param name="actions"></param>
-    public void BindChangeField(string key, IEnumerable<Action> actions)
-    {
-        List<Action> localActions = null;
-        if (bindedActions.TryGetValue(key, out localActions))
+        public static Data Instance
         {
-            localActions.AddRange(actions);
-            return;
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new Data();
+                    }
+                }
+                return instance;
+            }
         }
 
-        bindedActions[key] = new List<Action>(actions);
-    }
+        #region binding methods
 
-    /// <summary>
-    /// Удаляет все связанные действия по ключу key
-    /// </summary>
-    /// <param name="key">Ключ связки</param>
-    public void Unbind(string key)
-    {
-        List<Action> actions = null;
-        if (bindedActions.TryGetValue(key, out actions) && actions != null)
-            bindedActions[key] = new List<Action>();
-    }
-
-    private void InvokeAll(string key)
-    {
-        List<Action> actions = null;
-        if (bindedActions.TryGetValue(key, out actions))
-            actions?.ForEach(action => action?.Invoke());
-    }
-    #endregion
-
-    #region memory methods
-
-    /// <summary>
-    /// Записывает или изменяет значение по ключу key на value
-    /// </summary>
-    /// <param name="key">Ключ</param>
-    /// <param name="value">Объект, которые записывается или изменяется</param>
-    public void Set(string key, object value)
-    {
-        data[key] = value;
-        InvokeAll(key);
-    }
-
-    /// <summary>
-    /// Получает объект из данных
-    /// </summary>
-    /// <param name="key">Ключ</param>
-    /// <returns>Объект по ключу или null</returns>
-    public object Get(string key)
-    {
-        object value = null;
-        if (data.TryGetValue(key, out value))
-            return value;
-
-        return null;
-    }
-
-    /// <summary>
-    /// Получает объект из данных типа <c>T</c>
-    /// </summary>
-    /// <typeparam name="T">Тип возвращаемого значения</typeparam>
-    /// <param name="key">Ключ</param>
-    /// <param name="default">Значение по умолчание. В случае, если в данных нет объекта по ключу key</param>
-    /// <returns>Объект по ключу или @default</returns>
-    /// <exception cref="InvalidCastException"></exception>
-    public T Get<T>(string key, T @default = default(T))
-    {
-        object value = null;
-        if (data.TryGetValue(key, out value))
+        /// <summary>
+        /// Связывает выполнение действия action после изменения значения данных ко ключу key
+        /// в зависимости от значения triggerKind
+        /// </summary>
+        public void BindChangeField(string key, Action action)
         {
-            return (T) value;
+            BindChangeField(key, action, TriggerKind.After);
         }
 
-        return @default;
-    }
-
-    /// <summary>
-    /// Получает целое число из данных
-    /// </summary>
-    /// <param name="key">Ключ</param>
-    /// <param name="default">Значение по умолчание. В случае, если в данных нет объекта по ключу key</param>
-    /// <returns>Объект по ключу или @default</returns>
-    /// <exception cref="InvalidCastException"></exception>
-    public int GetInt(string key, int @default = 0)
-    {
-        object value = null;
-        if (data.TryGetValue(key, out value))
-            return (int) value;
-
-        return @default;
-    }
-
-    /// <summary>
-    /// Получает число с плавающей точкой из данных
-    /// </summary>
-    /// <param name="key">Ключ</param>
-    /// <param name="default">Значение по умолчание. В случае, если в данных нет объекта по ключу key</param>
-    /// <returns>Объект по ключу или @default</returns>
-    /// <exception cref="InvalidCastException"></exception>
-    public float GetFloat(string key, float @default = 0f)
-    {
-        object value;
-        if (data.TryGetValue(key, out value))
-            return (float)value;
-
-        return @default;
-    }
-
-    /// <summary>
-    /// Получает логическое значение из данных
-    /// </summary>
-    /// <param name="key">Ключ</param>
-    /// <param name="default">Значение по умолчание. В случае, если в данных нет объекта по ключу key</param>
-    /// <returns>Объект по ключу или @default</returns>
-    /// <exception cref="InvalidCastException"></exception>
-    public bool GetBool(string key, bool @default = false)
-    {
-        object value = null;
-        if (data.TryGetValue(key, out value))
-            return (bool) value;
-
-        return @default;
-    }
-
-    /// <summary>
-    /// Возвращает строку из данных
-    /// </summary>
-    /// <param name="key">Ключ</param>
-    /// <param name="default">Значение по умолчание. В случае, если в данных нет объекта по ключу key</param>
-    /// <returns>Строку, представляющий объект по ключу или @default</returns>
-    public string GetString(string key, string @default = "")
-    {
-        object value = null;
-        if (data.TryGetValue(key, out value))
-            return value.ToString();
-
-        return @default;
-    }
-
-    public Type GetValueType(string key)
-    {
-        object value = null;
-        return data.TryGetValue(key, out value) ? value.GetType() : (Type) null;
-    }
-
-    public void Increase(string key, int valueToIncrease = 1)
-    {
-        if (Instance.GetValueType(key) == typeof(int))
+        /// <summary>
+        /// Связывает выполнение действия action до или после изменения значения данных ко ключу key
+        /// в зависимости от значения triggerKind
+        /// </summary>
+        public void BindChangeField(string key, Action action, TriggerKind triggerKind)
         {
-            var obj = Instance.GetInt(key);
-            Instance.Set(key, obj + valueToIncrease);
-        }
-    }
+            DataInfo info = null;
+            if (data.TryGetValue(key, out info))
+            {
+                info.AddTriggers(action, triggerKind);
+                return;
+            }
 
-    public void Increase(string key, float valueToIncrease = 1f)
-    {
-        if (Instance.GetValueType(key) == typeof(float))
+            info = new DataInfo { Value = null };
+
+            info.AddTriggers(action, triggerKind);
+            data[key] = info;
+        }
+
+        /// <summary>
+        /// Удаляет все связанные действия по ключу key
+        /// </summary>
+        /// <param name="key">Ключ связки</param>
+        public void Unbind(string key)
         {
-            var obj = Instance.GetFloat(key);
-            Instance.Set(key, obj + valueToIncrease);
+            Unbind(key, TriggerKind.Before | TriggerKind.After);
         }
-    }
 
-    public void Increase(string key, double valueToIncrease = 1.0)
-    {
-        if (Instance.GetValueType(key) == typeof(double))
+        /// <summary>
+        /// Удаляет все связанные действия по ключу key
+        /// </summary>
+        /// <param name="key">Ключ связки</param>
+        public void Unbind(string key, TriggerKind triggerKind)
         {
-            var obj = Instance.Get<double>(key);
-            Instance.Set(key, obj + valueToIncrease);
+            DataInfo info = null;
+            if (data.TryGetValue(key, out info))
+            {
+                info.RemoveTriggers(triggerKind);
+            }
         }
-    }
 
-    public void Decrease(string key, int valueToDecrease = 1)
-    {
-        if (Instance.GetValueType(key) == typeof(int))
+        #endregion
+
+        #region memory methods
+
+        /// <summary>
+        /// Записывает или изменяет значение по ключу key на value
+        /// </summary>
+        /// <param name="key">Ключ</param>
+        /// <param name="value">Объект, которые записывается или изменяется</param>
+        public void Set(string key, object value)
         {
-            var obj = Instance.GetInt(key);
-            Instance.Set(key, obj - valueToDecrease);
-        }
-    }
+            DataInfo info = null;
+            if (data.TryGetValue(key, out info))
+            {
+                info.TriggersBefore?.InvokeAll();
+                info.Value = value;
+                info.TriggersAfter?.InvokeAll();
+                return;
+            }
 
-    public void Decrease(string key, float valueToDecrease = 1f)
-    {
-        if (Instance.GetValueType(key) == typeof(float))
+            info = new DataInfo { Value = value };
+            data[key] = info;
+        }
+
+        /// <summary>
+        /// Получает объект из данных
+        /// </summary>
+        /// <param name="key">Ключ</param>
+        /// <returns>Объект по ключу или null</returns>
+        public object Get(string key)
         {
-            var obj = Instance.GetFloat(key);
-            Instance.Set(key, obj - valueToDecrease);
-        }
-    }
+            DataInfo info = null;
+            if (data.TryGetValue(key, out info))
+                return info.Value;
 
-    public void Decrease(string key, double valueToDecrease = 1.0)
-    {
-        if (Instance.GetValueType(key) == typeof(double))
+            return null;
+        }
+
+        /// <summary>
+        /// Получает объект из данных типа <c>T</c>
+        /// </summary>
+        /// <typeparam name="T">Тип возвращаемого значения</typeparam>
+        /// <param name="key">Ключ</param>
+        /// <param name="default">Значение по умолчание. В случае, если в данных нет объекта по ключу key</param>
+        /// <returns>Объект по ключу или @default</returns>
+        /// <exception cref="InvalidCastException"></exception>
+        public T Get<T>(string key, T @default = default(T))
         {
-            var obj = Instance.Get<double>(key);
-            Instance.Set(key, obj - valueToDecrease);
+            DataInfo info = null;
+            if (data.TryGetValue(key, out info) && info.Value != null)
+            {
+                return (T)info.Value;
+            }
+
+            return @default;
         }
+
+        /// <summary>
+        /// Получает целое число из данных
+        /// </summary>
+        /// <param name="key">Ключ</param>
+        /// <param name="default">Значение по умолчание. В случае, если в данных нет объекта по ключу key</param>
+        /// <returns>Объект по ключу или @default</returns>
+        /// <exception cref="InvalidCastException"></exception>
+        public int GetInt(string key, int @default = 0)
+        {
+            DataInfo info = null;
+            if (data.TryGetValue(key, out info) && info.Value != null)
+                return (int)info.Value;
+
+            return @default;
+        }
+
+        /// <summary>
+        /// Получает число с плавающей точкой из данных
+        /// </summary>
+        /// <param name="key">Ключ</param>
+        /// <param name="default">Значение по умолчание. В случае, если в данных нет объекта по ключу key</param>
+        /// <returns>Объект по ключу или @default</returns>
+        /// <exception cref="InvalidCastException"></exception>
+        public float GetFloat(string key, float @default = 0f)
+        {
+            DataInfo info = null;
+            if (data.TryGetValue(key, out info) && info.Value != null)
+                return (float)info.Value;
+
+            return @default;
+        }
+
+        /// <summary>
+        /// Получает логическое значение из данных
+        /// </summary>
+        /// <param name="key">Ключ</param>
+        /// <param name="default">Значение по умолчание. В случае, если в данных нет объекта по ключу key</param>
+        /// <returns>Объект по ключу или @default</returns>
+        /// <exception cref="InvalidCastException"></exception>
+        public bool GetBool(string key, bool @default = false)
+        {
+            DataInfo info = null;
+            if (data.TryGetValue(key, out info) && info.Value != null)
+                return (bool)info.Value;
+
+            return @default;
+        }
+
+        /// <summary>
+        /// Возвращает строку из данных
+        /// </summary>
+        /// <param name="key">Ключ</param>
+        /// <param name="default">Значение по умолчание. В случае, если в данных нет объекта по ключу key</param>
+        /// <returns>Строку, представляющий объект по ключу или @default</returns>
+        public string GetString(string key, string @default = "")
+        {
+            DataInfo info = null;
+            if (data.TryGetValue(key, out info) && info.Value != null)
+                return info.Value.ToString();
+
+            return @default;
+        }
+
+        public Type GetValueType(string key)
+        {
+            DataInfo info = null;
+            return data.TryGetValue(key, out info) && info.Value != null ? info.Value.GetType() : (Type)null;
+        }
+
+        public void Increase(string key, int valueToIncrease = 1)
+        {
+            if (GetValueType(key) == typeof(int))
+            {
+                var obj = GetInt(key);
+                Set(key, obj + valueToIncrease);
+            }
+        }
+
+        public void Increase(string key, float valueToIncrease = 1f)
+        {
+            if (GetValueType(key) == typeof(float))
+            {
+                var obj = GetFloat(key);
+                Set(key, obj + valueToIncrease);
+            }
+        }
+
+        public void Increase(string key, double valueToIncrease = 1.0)
+        {
+            if (GetValueType(key) == typeof(double))
+            {
+                var obj = Get<double>(key);
+                Set(key, obj + valueToIncrease);
+            }
+        }
+
+        public void Decrease(string key, int valueToDecrease = 1)
+        {
+            if (GetValueType(key) == typeof(int))
+            {
+                var obj = GetInt(key);
+                Set(key, obj - valueToDecrease);
+            }
+        }
+
+        public void Decrease(string key, float valueToDecrease = 1f)
+        {
+            if (GetValueType(key) == typeof(float))
+            {
+                var obj = GetFloat(key);
+                Set(key, obj - valueToDecrease);
+            }
+        }
+
+        public void Decrease(string key, double valueToDecrease = 1.0)
+        {
+            if (GetValueType(key) == typeof(double))
+            {
+                var obj = Get<double>(key);
+                Set(key, obj - valueToDecrease);
+            }
+        }
+
+        #endregion
     }
 
-    #endregion
+    internal static class Extentions
+    {
+        public static bool TriggerHasFlag(this TriggerKind triggerKind, TriggerKind flag)
+        {
+            // Faster than native Enum.HasFlag
+            return (triggerKind & flag) == flag;
+        }
+
+        public static void InvokeAll(this IEnumerable<Action> actions)
+        {
+            if (actions == null)
+                return;
+
+            foreach (var action in actions)
+            {
+                action?.Invoke();
+            }
+        }
+    }
 }
